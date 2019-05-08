7edce19cd22fcc161fb18ec9e8718ce5f3154cc1
JoSeBu1 <48152124+JoSeBu1@users.noreply.github.com>
2019-05-08 18:52:00 +0200

    Commit Inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  user: any = {};
+  showUser: boolean = false;
 
+  constructor(public navCtrl: NavController, private loadingController: LoadingController, private googlePlus: GooglePlus) {
+
+  }
+
+  async doGoogleLogin(){
+  	const loading = await this.loadingController.create({
+  		content: 'Please wait...'
+  	});
+  	this.presentLoading(loading);
+  
+  	this.googlePlus.login({
+  		'scopes': 'profile', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+  		'webClientId': '678325328944-cm2ml3tuee13alvgme5b7qlrcuocb8ll.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+  		'offline': true // Optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
+  	})
+  	.then(user =>{
+      this.user = user;
+      this.showUser = true; 
+      loading.dismiss();
+  	}, err =>{
+  		console.log(err)
+      loading.dismiss();
+      alert(err);
+  	});
+  
+  }
+
+  async presentLoading(loading) {
+    return await loading.present();
+  }
+
+  doGoogleLogout(){
+    this.googlePlus.logout()
+    .then(res =>{
+    }, err =>{
+      console.log(err);
+    })
   }
 
 }
